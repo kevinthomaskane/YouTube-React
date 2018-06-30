d065a336cced1b42a6de8b2215515fd34619e846
Travis Kane <kevinthomaskane@gmail.com>
2018-06-30 11:35:48 -0700

    implement lodash

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
 import VideoDetail from "./components/video_detail";
 import YTSearch from "youtube-api-search";
+import _ from "lodash";
 
 const API_KEY = "AIzaSyDhMbK-iWOty8oShHQInwdWuZsBmOKq2j8";
 
@@ -16,25 +17,28 @@ class App extends Component {
   };
 
   componentDidMount() {
-    this.videoSearch("kodak black")
+    this.videoSearch("kodak black");
   }
-    
 
-  videoSearch = (term) => {
+  videoSearch = term => {
     YTSearch({ key: API_KEY, term: term }, videos => {
       this.setState({ videos: videos, selectedVideo: videos[0] });
     });
-  }
-  
+  };
 
   render() {
+    const videoSearch = _.debounce(term => {
+      this.videoSearch(term);
+    }, 300);
+
     return (
       <div>
-        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+        <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo} />
-        <VideoList 
-        onVideoSelect={selectedVideo => this.setState({selectedVideo})}
-        videos={this.state.videos} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
